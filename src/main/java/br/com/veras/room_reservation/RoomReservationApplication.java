a082378a35827511797e3c714928f819c2637dd1
Lucas Veras <lucasverasspineli@gmail.com>
2025-02-07 23:02:20 -0300

    Criando projeto

---
+++ src/main/java/br/com/veras/room_reservation/RoomReservationApplication.java
@@ -0,0 +1,13 @@
+package br.com.veras.room_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationApplication.class, args);
+	}
+
+}
