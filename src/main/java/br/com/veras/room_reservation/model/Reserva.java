b7fe6ad501aaa93288c63f5ca1e4f2a7d506aadc
Lucas Veras <lucasverasspineli@gmail.com>
2025-03-27 00:12:41 -0300

    Criei a entidade Reserva

---
+++ src/main/java/br/com/veras/room_reservation/model/Reserva.java
@@ -0,0 +1,46 @@
+package br.com.veras.room_reservation.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_reserva")
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numeroReserva;
+    @Column(nullable = false)
+    private LocalDateTime dataHora;
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "login_user", referencedColumnName = "login"),
+            @JoinColumn(name = "senha_user", referencedColumnName = "senha")
+    })
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "sala_id", nullable = false)
+    private Sala sala;
+
+    public Reserva() {
+    }
+
+    public Reserva(LocalDateTime dataHora, Sala sala, User user) {
+        this.dataHora = dataHora;
+        this.sala = sala;
+        this.user = user;
+    }
+
+    public Reserva(LocalDateTime dataHora, Long id_sala, String login, String senha){
+        this.dataHora = dataHora;
+        sala.setId(id_sala);
+        user.setUserId(new UserId(login,senha));
+    }
+
+
+}
