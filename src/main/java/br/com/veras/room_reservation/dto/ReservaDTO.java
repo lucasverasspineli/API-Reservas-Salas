b3917cb06e21e9b85c094fd2f9a87ba9bb812290
Lucas Veras <lucasverasspineli@gmail.com>
2025-05-12 21:26:54 -0300

    Remodelagem da classe ReservaDTO

--- src/main/java/br/com/veras/room_reservation/dto/ReservaDTO.java
+++ src/main/java/br/com/veras/room_reservation/dto/ReservaDTO.java
@@ -1,38 +1,24 @@
 package br.com.veras.room_reservation.dto;
 
 import br.com.veras.room_reservation.model.Reserva;
-import br.com.veras.room_reservation.model.Sala;
-import br.com.veras.room_reservation.model.User;
-import br.com.veras.room_reservation.model.UserId;
-import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class ReservaDTO {
 
     private Long numeroReserva;
     private LocalDateTime dataHora;
-    private User user;
-    private Sala sala;
+    private UserMinDTO user;
+    private SalaMinDTO sala;
 
     public ReservaDTO(){}
 
-    public ReservaDTO(LocalDateTime dataHora, Long numeroReserva, Sala sala, User user) {
-        this.dataHora = dataHora;
-        this.numeroReserva = numeroReserva;
-        this.sala = sala;
-        this.user = user;
-    }
+    public ReservaDTO(Reserva r) {
+        this.dataHora = r.getDataHora();
+        this.numeroReserva = r.getNumeroReserva();
+        this.sala = new SalaMinDTO(r.getSala());
+        this.user = new UserMinDTO(r.getUser());
 
-    public ReservaDTO(Reserva entity){
-        BeanUtils.copyProperties(entity,this);
-    }
-
-    public void ReservaDtoForReserva(LocalDateTime dataHora, Long id_sala, String login, String senha){
-        this.dataHora = dataHora;
-        sala.setId(id_sala);
-        user.setUserId(new UserId(login,senha));
     }
 
     public LocalDateTime getDataHora() {
@@ -51,31 +37,20 @@ public class ReservaDTO {
         this.numeroReserva = numeroReserva;
     }
 
-    public Sala getSala() {
+    public SalaMinDTO getSalaMin() {
         return sala;
     }
 
-    public void setSala(Sala sala) {
+    public void setSalaMin(SalaMinDTO sala) {
         this.sala = sala;
     }
 
-    public User getUser() {
+    public UserMinDTO getUserMin() {
         return user;
     }
 
-    public void setUser(User user) {
+    public void setUserMin(UserMinDTO user) {
         this.user = user;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof ReservaDTO that)) return false;
-        return Objects.equals(numeroReserva, that.numeroReserva);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(numeroReserva);
-    }
 }
